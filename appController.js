37731561a0a814735815ec2d34d8034c20132632
Sonu Jose <sjose@valorem.com>
2017-05-03 16:02:19 +0530

    Edit Functionality added for contents

--- appController.js
+++ appController.js
@@ -1,10 +1,11 @@
 
 var app = angular.module('myApp', []);
-app.controller('mainAppCtrl', function($scope) {
-    $scope.name = "John Doe";
+app.controller('mainAppCtrl', function($scope,$sce) {
+    
     $scope.newItem = {'type' : 'para' , 'data' : ''};
     $scope.newContentList = [];
     $scope.generatedJsonContent = JSON.stringify($scope.newContentList);
+    $scope.isEditMode = false;
 
     $scope.scrollToBottom = function() {
         window.scrollTo(0,document.body.scrollHeight);
@@ -16,7 +17,43 @@ app.controller('mainAppCtrl', function($scope) {
         var currentLength = $scope.newContentList.length;
         var object = {'id': currentLength + 1, 'type' : $scope.newItem.type, 'data' : $scope.newItem.data};
         $scope.newContentList.push(object);
-        $scope.generatedJsonContent = JSON.stringify($scope.newContentList);
+        $scope.generatedJsonContent = JSON.stringify($scope.newContentList, undefined, 2);
         $scope.resetItem();
     };
+
+    $scope.UpdateItem = function() {
+        $scope.newContentList.some(function(item){
+            if(item === $scope.selectedItem) {
+                item.data = $scope.newItem.data;                
+                return true;
+            }
+        });
+        $scope.generatedJsonContent = JSON.stringify($scope.newContentList, undefined, 2);
+        $scope.isEditMode = false;
+        $scope.selectedItem = {};
+        $scope.resetItem();
+    } 
+
+    $scope.trustContentAsHtml = function(data) {
+        return $sce.trustAsHtml(data);
+    }
+
+    $scope.editContent = function (dataVal) {
+        $scope.newContentList.some(function(item){
+            if(item.id === dataVal) {
+                $scope.selectedItem = item;
+                return true;
+            }
+        });
+        
+        $scope.newItem = $scope.selectedItem;
+        $scope.isEditMode = true;
+    }
+
+    $scope.resetEntireList = function() {
+        $scope.newContentList = [];
+        $scope.generatedJsonContent = JSON.stringify($scope.newContentList, undefined, 2);
+        $scope.resetItem();
+    }
+
 });
